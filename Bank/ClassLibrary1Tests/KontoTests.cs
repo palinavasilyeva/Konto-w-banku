a2d79415e1bbac8ddfb69ec7e590318043df7e2f
palinavasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-04-03 19:06:26 +0200

    Add tests

--- Bank/ClassLibrary1Tests/KontoTests.cs
+++ Bank/ClassLibrary1Tests/KontoTests.cs
@@ -95,6 +95,42 @@ namespace ClassLibrary1.Tests
             // Assert
             Assert.IsTrue(konto.Zablokowane);
         }
+
+        [TestMethod()]
+        public void TestWyplataNegatywnaKwota()
+        {
+            // Arrange
+            var konto = new Konto("Jan Kowalski", 1000m);
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => konto.Wyplata(-500m));
+            Assert.ThrowsException<ArgumentException>(() => konto.Wyplata(0m));
+        }
+
+        [TestMethod()]
+        public void TestUstawienieNowegoBilansu()
+        {
+            // Arrange
+            var konto = new Konto("Jan Kowalski", 1000m);
+
+            // Act
+            konto.UstawBilans(500m);
+
+            // Assert
+            Assert.AreEqual(500m, konto.Bilans);
+        }
+
+        [TestMethod()]
+        public void TestWyplataBrakSrodkow()
+        {
+            // Arrange
+            var konto = new Konto("Jan Kowalski", 1000m);
+
+            // Act & Assert
+            Assert.ThrowsException<InvalidOperationException>(() => konto.Wyplata(2000m));  // Insufficient funds
+        }
+
+
     }
 
     [TestClass()]
@@ -152,6 +188,37 @@ namespace ClassLibrary1.Tests
             // Act & Assert
             Assert.ThrowsException<InvalidOperationException>(() => konto.WyplataKontoPlus(2001m));
         }
+
+        [TestMethod()]
+        public void TestWplataPoDebecieNaKontoPlus()
+        {
+            // Arrange
+            var konto = new KontoPlus("Jan Kowalski", 1000m, 500m);
+            konto.WyplataKontoPlus(1500m); 
+
+            // Act
+            konto.WplataKontoPlus(2000m);
+
+            // Assert
+            Assert.AreEqual(2000m, konto.EffectiveBilans);
+            Assert.IsFalse(konto.Zablokowane);
+        }
+
+        [TestMethod()]
+        public void TestResetowanieDebetuPoWplacie()
+        {
+            // Arrange
+            var konto = new KontoPlus("Jan Kowalski", 1000m, 500m);
+            konto.WyplataKontoPlus(1500m); // Use overdraft
+
+            // Act
+            konto.WplataKontoPlus(1000m); // Deposit should clear the overdraft
+
+            // Assert
+            Assert.AreEqual(1000m, konto.EffectiveBilans);  // Effective balance should be back to the deposited amount
+            Assert.IsFalse(konto.Zablokowane);  // The account should be unblocked
+        }
+
     }
 
     [TestClass()]
@@ -184,5 +251,30 @@ namespace ClassLibrary1.Tests
             Assert.AreEqual(0m, konto.Bilans);
             Assert.IsTrue(konto.Zablokowane);
         }
+
+        [TestMethod()]
+        public void TestPrzekroczenieLimituDebetowegoKontoLimit()
+        {
+            // Arrange
+            var konto = new KontoLimit("Jan Kowalski", 1000m, 500m);
+
+            // Act & Assert
+            Assert.ThrowsException<InvalidOperationException>(() => konto.WyplataKontoLimit(2001m));
+        }
+
+        [TestMethod()]
+        public void TestWyplataZKontemLimitBezUzyciaDebetu()
+        {
+            // Arrange
+            var konto = new KontoLimit("Jan Kowalski", 1000m, 500m);
+
+            // Act
+            konto.WyplataKontoLimit(1000m);  // Should use balance, not the overdraft
+
+            // Assert
+            Assert.AreEqual(0m, konto.Bilans); // Balance should be zero
+            Assert.IsFalse(konto.Zablokowane); // The account should not be blocked since no overdraft was used
+        }
+
     }
 }
